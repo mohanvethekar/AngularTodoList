1f8d98355f1a61d7b5b19fb801d4335b0e4cab20
mohanvethekar <iammohanvethekar001>
2023-12-13 13:52:53 +0530

    ..

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,17 +3,17 @@ import { CommonModule } from '@angular/common';
 import { Form , FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
+
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule , FormsModule , RouterLink],
+  imports: [CommonModule , FormsModule , RouterLink ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
 
   isUserLoggedIn : boolean = false ;
-
   constructor( private HardcodedAuthenticationService : HardcodedAuthenticationService){
     this.isUserLoggedIn = this.HardcodedAuthenticationService.isUserLoggedIn();
   }
--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -1,10 +1,4 @@
 import { Injectable } from '@angular/core';
-import { LoginComponent } from '../login/login.component';
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Form, FormsModule } from '@angular/forms';
-import { ActivatedRoute, RouterLink } from '@angular/router';
-
 @Injectable({
   providedIn: 'root'})
 export class HardcodedAuthenticationService {
